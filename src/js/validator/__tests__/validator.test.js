c5255cfe555901e459fdd5954181276b8df6c627
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-10-05 20:39:15 +0300

    add validator with tests

---
+++ src/js/validator/__tests__/validator.test.js
@@ -0,0 +1,19 @@
+import Validator from '../validator';
+
+const invalidNicknames = [
+  ['1nickname'],
+  ['-nickname'],
+  ['_nickname'],
+  ['nickname1'],
+  ['nickname-'],
+  ['nickname_'],
+  ['nick7777name'],
+];
+
+test.each(invalidNicknames)('Expect .validateNickname to return false with %s', (invalidNickname) => {
+  expect(Validator.validateUsername(invalidNickname)).toBe(false);
+});
+
+test('Expect .validateNickname to return true with valid nickname', () => {
+  expect(Validator.validateUsername('some_nick333-22name')).toBe(true);
+});
--- src/js/validator/validator.js
+++ src/js/validator/validator.js
@@ -1,3 +1,8 @@
 export default class Validator {
-
+  static validateUsername(username) {
+    if (/^[^\d\s-_][-\w]+[^\d\s-_]$/.test(username) && !/\d{4,}/.test(username)) {
+      return true;
+    }
+    return false;
+  }
 }
